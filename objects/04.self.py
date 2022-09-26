88234d25fcea8582d04006caf037261bac4e82c6
Camila munoz <cmunoz@bpo.cl>
2022-09-26 10:55:13 -0300

    dictionaries

--- objects/04.self.py
+++ objects/04.self.py
@@ -3,3 +3,22 @@
 # palabra reservada class
 # 
 
+first_dict = {
+  "name": "students",
+  "position": "Fullstack Developer",
+  "skills": ["Python", "Git", "HTML","CSS","Javascript"]
+}
+class Student:
+    def __init__(self,name,position,skills):
+        self.name = name
+        self.position = position
+        self.skills = skills
+
+    def say_name(self):
+        print("Mi nombre es", self.name,"mi cargo es",self.position,"mis habilidades son",self.
+        skills)
+
+Alice = Student("Alice","Fullstack Developer",["Python", "Git", "HTML", "CSS", "Javascript"])
+Bob_el_chef = Student("Bob el chef","Ayudante de cocina",["Pasteleria y cocina fria"])
+Alice.say_name()
+Bob_el_chef.say_name()
